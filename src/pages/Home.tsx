ca898ba772196a14cba8674a3cddc0a7f3ce54dd
naokihash <git@stackblitz.com>
2024-11-04 23:32:19 +0900

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import Menu from '../components/Menu';
+import InstagramFeed from '../components/InstagramFeed';
+import About from '../components/About';
+import Footer from '../components/Footer';
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-[#f7f5f0]">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Menu />
+      <InstagramFeed />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
